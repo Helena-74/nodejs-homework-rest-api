0c829b39f0cc8681e234e37af8feb9c2e7d58122
Helena-74 <122282363+Helena-74@users.noreply.github.com>
2023-10-26 20:19:28 +0200

    commit

--- models/contact.js
+++ models/contact.js
@@ -38,7 +38,7 @@ const contactSchema = new Schema({
   owner:{
     type: Schema.Types.ObjectId,
     ref: "user",
-    required: true,
+    // required: true,
   }
 }, {
   versionKey: false, timestamps: true
